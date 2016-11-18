d4b29640c60c4749bb426fac70122043c9109b52
Nemanja3089 <nemanjakuzmanovic1989@gmail.com>
2016-11-18 18:25:55 +0100

    Update index.js

--- Client/index.js
+++ Client/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory} from 'react-router';
 import routes from './routes';
-
 import 'babel-polyfill';
-
-
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
-
-
-
 
 const store = createStore(
     rootReducer,
