d0fb5a09a4b925638f8170a9874488d3cdc86d29
Nemanja3089 <nemanjakuzmanovic1989@gmail.com>
2016-12-08 16:53:06 +0100

    Client Uniqueness Validation

--- Client/components/signup/SignupForm.js
+++ Client/components/signup/SignupForm.js
@@ -17,10 +17,12 @@ class SignupForm extends React.Component {
       passwordConfirmation: '',
       timezone: '',
       errors:{},
-      isLoading: false
+      isLoading: false,
+      invalid: false
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.checkUserExists = this.checkUserExists.bind(this);
   }
   onChange(e){
     this.setState({[e.target.name]:e.target.value});
@@ -32,6 +34,25 @@ class SignupForm extends React.Component {
     }
     return isValid;
   }
+  checkUserExists(e) {
+    const field = e.target.name;
+    const val = e.target.value;
+    if (val !== '') {
+      this.props.isUserExists(val).then(res => {
+        let errors = this.state.errors;
+        let invalid;
+        if (res.data.user) {
+          errors[field] = 'There is user with such ' + field;
+          invalid = true;
+        } else {
+          errors[field] = '';
+          invalid = false;
+        }
+        this.setState({ errors, invalid });
+      });
+    }
+}
+
   onSubmit(e){
 
     e.preventDefault();
@@ -111,7 +132,7 @@ class SignupForm extends React.Component {
           </div>
 
            <div className="form-group">
-           <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">Sign up</button>
+           <button disabled={this.state.isLoading || this.state.invalid} className="btn btn-primary btn-lg">Sign up</button>
            </div>
 
       </form>
@@ -121,7 +142,8 @@ class SignupForm extends React.Component {
 }
 SignupForm.propTypes = {
   userSignupRequest:React.PropTypes.func.isRequired,
-  addFlashMessage:React.PropTypes.func.isRequired
+  addFlashMessage:React.PropTypes.func.isRequired,
+  isUserExists: React.PropTypes.func.isRequired
 };
 SignupForm.contextTypes = {
   router:React.PropTypes.object.isRequired
